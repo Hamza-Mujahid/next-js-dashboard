a17076d543cda9890cb07195607a5a1c563e72ac
Muhammad Hamza <80825408+Hamza-Mujahid@users.noreply.github.com>
2024-10-09 11:48:04 -0500

    Working Nextjs-DashBoard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+    return (
+        <div>
+            This is the dashboard!
+            <br />
+            <p className='text-9xl'>Ollo !!</p>
+        </div>
+    )
+}
+
+export default Page
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Raleway, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const raleway = Raleway({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' })
